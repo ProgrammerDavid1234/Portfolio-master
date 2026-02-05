336129b8a62eff5abf699f2f0d32b75b573751af
ProgrammerDavid1234 <olonadenifemi@gmail.com>
2026-02-05 22:11:32 +0100

    m

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -55,7 +55,7 @@ function NavBar() {
           <Nav className="ms-auto" defaultActiveKey="#home">
             <Nav.Item>
               <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Homeses
+                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
               </Nav.Link>
             </Nav.Item>
 
